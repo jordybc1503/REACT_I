655824c553efa0cd359da35407ae316cbffcc556
Jordi Bolo <152198990+jordybc1503@users.noreply.github.com>
2024-06-24 19:53:10 -0500

    Actividad - Implementar clases y prefijos de Tailwind en Cart

--- src/views/Cart.tsx
+++ src/views/Cart.tsx
@@ -1,4 +1,4 @@
-import styles from "./Cart.module.css";
+
 import Footer from "../components/Footer";
 import NavBar from "../components/NavBar";
 import Hero from "../components/Hero";
@@ -8,17 +8,17 @@ function Cart() {
     <>
       <NavBar />
       <Hero first="mi" second="carrito" />
-      <main>
-        <article className={styles["product-cart"]}>
+      <main className="w-full flex justify-center items-center p-5">
+        <article className="bg-[#f2f2f2] rounded-[5px] p-[30px] m-2.5 h-[220px] w-[680px] break-words flex  justify-between items-center">
           <img
-            className={styles["product-img"]}
+            className="w-[680px] h-[100px] rounded-md"
             src="https://i.postimg.cc/kX8PKZpq/ipad.jpg"
             alt="ipad"
           />
-          <div className={styles["product-details"]}>
-            <strong className={styles["product-title"]}>iPad Pro 13</strong>
-            <span className={styles["product-description"]}>- Silver</span>
-            <p className={styles["product-description"]}>
+          <div className="flex flex-col justify-between gap-0.5 w-85 h-25">
+            <strong className="font-bold">iPad Pro 13</strong>
+            <span className="whitespace-nowrap overflow-hidden overflow-ellipsis">- Silver</span>
+            <p className="whitespace-nowrap overflow-hidden overflow-ellipsis">
               The iPad Pro 13 is a stunning piece of technology, boasting a
               large 12.9-inch Retina display with ProMotion technology. With
               256GB of storage, this iPad provides ample space for all your
@@ -31,7 +31,7 @@ function Cart() {
               the iPad Pro 12.9 is a versatile device that meets all your needs.
             </p>
             <input
-              className={styles["product-input"]}
+              className="w-18 h-10 rounded-md border border-gray-200 p-1"
               type="number"
               name="quantity"
               defaultValue="1"
@@ -39,24 +39,24 @@ function Cart() {
               id="P7Q8R90"
             />
           </div>
-          <strong className={styles["price"]}>AR$ $800000</strong>
+          <strong className="w-25 text-right">AR$ $800000</strong>
         </article>
-        <div className={styles["cart-resume"]}>
-          <div className={styles["cart-data"]}>
-            <h2 className={styles["cart-title"]}>
+        <div className="w-[340px] flex-col bg-[#f2f2f2] rounded-[5px] p-[30px] m-2.5 h-[220px] break-words flex  justify-between">
+          <div className="flex-grow flex flex-col justify-between">
+            <h2 className="flex justify-between">
               <span>Resumen</span>
               <span>del</span>
               <span>pedido</span>
             </h2>
-            <div className={styles["cart-total"]}>
+            <div className="flex justify-between items-center">
               <h3>Total</h3>
-              <strong className={styles["cart-price"]}>$800000</strong>
+              <strong >$800000</strong>
             </div>
-            <small className={styles["cart-tax"]}>
+            <small className="pb-2.5">
               Incluye impuesto PAIS y percepción AFIP.
             </small>
           </div>
-          <button className={styles["cart-btn"]} id="buy" type="button">
+          <button className="w-full bg-[#ff3b3c] text-white font-bold border-none h-10 rounded-lg hover:bg-[ff5151]" id="buy" type="button">
             COMPRAR
           </button>
         </div>
