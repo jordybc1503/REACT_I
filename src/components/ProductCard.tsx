0bfb17cb8068de10c1d90708989d8054f6bc68ca
Jordi Bolo <152198990+jordybc1503@users.noreply.github.com>
2024-06-25 00:57:55 -0500

    Actividad - Analizar y generar la interfaz del producto

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,19 +1,30 @@
-import styles from "./ProductCard.module.css";
+import Product from "../interfaces/ProductCard";
 import { Link } from "react-router-dom";
 
-export default function ProductCard(props) {
+export default function ProductCard(props: Product) {
   const { id, title, price, color, image } = props;
   return (
-    <Link className={styles["product-card"]} to={"/details/" + id}>
-      <img className={styles["product-img"]} src={image} alt={title} />
-      <div className={styles["product-info"]}>
-        <span className={styles["product-title"]}>{title}</span>
-        <span className={styles["product-description"]}>{color}</span>
-        <div className={styles["product-price-block"]}>
-          <span className={styles["product-price"]}>{price}</span>
-          <span className={styles["product-discount"]}>50% Off</span>
+    <Link
+      className="bg-[#f0f0f0] text-[#383838] rounded-md w-[250px] m-[15px] no-underline"
+      to={"/details/" + id}
+    >
+      <img
+        className="w-full h-[250px] object-cover object-center rounded-t-md"
+        src={image}
+        alt={title}
+      />
+      <div className="p-[20px]">
+        <span className="text-[20px] font-bold leading-[25px] block">
+          {title}
+        </span>
+        <span className="text-[12px] text-[#383838] mb-[5px]">{color}</span>
+        <div className="flex my-[5px] justify-between items-center">
+          <span className="text-[20px] font-bold text-[#428f13]">
+            $ {price}
+          </span>
+          <span className="text-[12px]">50% Off</span>
         </div>
-        <div className={styles["product-tax-policy"]}>
+        <div className="text-[10px]">
           Includes Country tax and AFIP collection
         </div>
       </div>
---
+++ src/interfaces/ProductCard.ts
@@ -0,0 +1,8 @@
+export default interface ProductCard {
+    id: string;
+    title: string;
+    price: number;
+    image: string;
+    color: string;
+
+}
