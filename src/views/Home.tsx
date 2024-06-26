89adef8f3716907f3bdda737b5a1a71215ed005a
Jordi Bolo <152198990+jordybc1503@users.noreply.github.com>
2024-06-25 20:39:16 -0500

    Actividad - Consumir JSON en Home

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -2,17 +2,28 @@ import Footer from "../components/Footer";
 import Hero from "../components/Hero";
 import NavBar from "../components/NavBar";
 import ProductCard from "../components/ProductCard";
-import products from "../assets/products";
-import product from "../interfaces/products";
+import Product from "../interfaces/products";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function Home() {
+
+  const [products, setProducts] = useState<Product[]>([]);
+  
+  useEffect(() => {
+    axios.get("/products.json")
+       .then((res) => setProducts(res.data))
+       .catch((err) => console.log(err));
+  }, []);
+ 
+
   return (
     <>
       <NavBar />
       <Hero first="tecnologia" second="renovada" />
       <main className="w-full flex justify-center items-center p-[20px]">
         <div className="w-[1080px] flex flex-wrap justify-between" id="products">
-          {products.map((each: product) => (
+          {products.map((each: Product) => (
             <ProductCard
               key={each.id}
               id={each.id}
