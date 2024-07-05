b1e3a98b84732355f558eae68fa597b3b65722d3
Jordi Bolo <152198990+jordybc1503@users.noreply.github.com>
2024-07-04 20:08:02 -0500

    Actividad - Configurar el almacenamiento de estados globales

---
+++ src/store/actions/products.ts
@@ -0,0 +1,14 @@
+import { createAction } from "@reduxjs/toolkit";
+
+//objeto para accion de capturas texto
+const captureText = createAction(
+    "captureText",
+    (obj)=> {
+        return {
+            payload: {text: obj.text}
+        }
+    }
+)
+
+const productsActions = {captureText} //objeto con todas las acciones
+export default productsActions;
---
+++ src/store/index.ts
@@ -0,0 +1,14 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+// variable para el almacenamiento de todas
+const store = configureStore(
+    {
+        reducer: {
+            //users,
+            //products
+        },
+    }
+);
+
+export default store;
+
